87878f738d9b3861039c3b454b62706922f9b7c2
Developpeur CPAM 1 <user@cpam.com>
2019-10-23 16:50:34 +0200

    Initial commit

---
+++ wp-content/themes/cpam-one/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *  @name index.php
+ *  @author Vaelia
+ * @version 1.0.0
+ * @abstract Main file for cpam-one theme
+ */
+?>
+<div class="container-fluid"
+    <header>
+        <h1><?php bloginfo("title");?> </h1>
+        <h2><?php bloginfo("description");?> </h2>
+    </header>
+</div>
